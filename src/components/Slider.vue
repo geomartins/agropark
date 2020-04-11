0fa764563fd5d45cfae6c80dec10bb2cd14ffaac
Martins Abiodun <martinsabiodun94@gmail.com>
2020-04-11 16:34:32 +0100

    modified slider (testing purposes)

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -7,7 +7,197 @@
             <!-- START REVOLUTION SLIDER 5.4.8 auto mode -->
             <div id="rev_slider_4_2" class="rev_slider fullwidthabanner" data-version="5.4.8.2">
                 <ul>
-                    <li data-index="rs-8" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+
+
+                    
+                    <li data-index="rs-6" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="images/slides/slider-mainbg-001.jpg" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+
+                        <img src="/img/slide1.png" alt="" title="mainslider-bg001" width="1920" height="750" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+
+                        <div class="tp-caption tp-resizeme" id="slide-6-layer-1" data-x="['left','left','left','left']" data-hoffset="['50','40','-220','-220']" data-y="['top','top','top','top']" data-voffset="['160','158','63','63']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-visibility="['on','on','off','off']"
+                        data-type="image"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":180,"speed":500,"frame":"0","from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]">
+
+                            <img src="images/slides/slider-icon001.png" alt="" data-ww="['113px','113px','113px','113px']" data-hh="['45px','45px','45px','45px']" width="113" height="45" data-no-retina> 
+
+                        </div>
+
+                        <div class="tp-caption slide-main-text tp-resizeme" id="slide-6-layer-2" data-x="['left','left','left','center']" data-hoffset="['175','163','30','0']" data-y="['top','top','top','middle']" data-voffset="['178','175','76','-115']"
+                        data-fontsize="['16','16','15','13']"
+                        data-lineheight="['16','16','15','13']"
+                        data-fontweight="['400','400','400','300']"
+                        data-color="['rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)']"
+                        data-letterspacing="1"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":330,"speed":500,"frame":"0","from":"x:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]">WE GIVE YOU THE BEST! </div>
+
+                        <div class="tp-caption slide-main-text tp-resizeme" id="slide-6-layer-3" data-x="['left','left','left','center']" data-hoffset="['50','40','30','0']" data-y="['top','top','top','middle']" data-voffset="['213','211','106','-75']"
+                        data-fontsize="['75','80','60','40']"
+                        data-lineheight="['80','80','55','40']"
+                        data-fontweight="['600','600','600','600']"
+                        data-color="['rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":470,"speed":800,"frame":"0","from":"x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]"><strong class="ttm-textcolor-skincolor">Medical</strong> Services </div>
+
+
+                        <div class="tp-caption slide-main-text tp-resizeme" id="slide-6-layer-4" data-x="['left','left','left','center']" data-hoffset="['50','40','30','0']" data-y="['top','top','top','middle']" data-voffset="['286','286','167','-33']"
+                        data-fontsize="['75','80','60','40']"
+                        data-lineheight="['80','80','60','40']"
+                        data-fontweight="['600','600','600','600']"
+                        data-color="['rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":900,"speed":800,"frame":"0","from":"x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]">That You Can <strong class="ttm-textcolor-skincolor">Trust</strong> </div>
+
+                        <div class="tp-caption slide-main-text tp-resizeme" id="slide-6-layer-5" data-x="['left','left','left','center']" data-hoffset="['50','40','30','0']" data-y="['top','top','top','middle']" data-voffset="['373','382','236','19']"
+                        data-fontsize="['16','16','15','12']"
+                        data-lineheight="['25','25','26','18']"
+                        data-fontweight="['400','400','400','400']"
+                        data-color="['rgb(237, 237, 237)','rgb(237, 237, 237)','rgb(237, 237, 237)','rgb(237, 237, 237)']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":1100,"speed":500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','center']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]">Need professional help? Our support staff will answer your questions.<br> Visit us Now or Make an Appointment! </div>
+
+                        <div class="tp-caption shape-rounded tp-resizeme" id="slide-6-layer-7" data-x="['center','center','center','center']" data-hoffset="['544','446','332','332']" data-y="['middle','middle','middle','middle']" data-voffset="['219','240','150','150']"
+                        data-width="50"
+                        data-height="48"
+                        data-fontsize="['23','23','23','23']"
+                        data-lineheight="['48','48','48','48']"
+                        data-fontweight="['400','400','400','400']"
+                        data-color="['rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)']"
+                        data-whitespace="nowrap"
+                        data-visibility="['on','on','on','off']"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":1240,"speed":500,"frame":"0","from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['center','center','center','center']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]"><span class=" fa fa-user-md"></span> </div>
+
+                        <div class="tp-caption icon-text tp-resizeme" id="slide-6-layer-8" data-x="['center','center','center','center']" data-hoffset="['444','349','235','442']" data-y="['middle','middle','middle','middle']" data-voffset="['207','227','138','95']"
+                        data-fontsize="['19','19','19','18']"
+                        data-lineheight="['21','21','21','20']"
+                        data-fontweight="['400','400','400','400']"
+                        data-color="['rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-visibility="['on','on','on','off']"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":1350,"speed":500,"frame":"0","from":"x:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]">Dr. Alex Murry </div>
+
+                        <div class="tp-caption icon-desc tp-resizeme" id="slide-6-layer-9" data-x="['center','center','center','center']" data-hoffset="['352','259','152','360']" data-y="['middle','middle','middle','middle']" data-voffset="['231','251','162','125']"
+                        data-fontsize="['15','15','14','13']"
+                        data-lineheight="['15','15','15','13']"
+                        data-fontweight="['400','400','400','400']"
+                        data-color="['rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)','rgb(255,255,255)']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-visibility="['on','on','on','off']"
+                        data-type="text"
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":1570,"speed":300,"frame":"0","from":"x:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]">Get an Appointment</div>
+
+                        <a class="tp-caption icon-btn tp-resizeme" href="#" target="_self" id="slide-6-layer-10" data-x="['center','center','center','center']" data-hoffset="['466','373','259','465']" data-y="['middle','middle','middle','middle']" data-voffset="['230','250','161','123']"
+                        data-fontsize="['15','15','14','13']"
+                        data-lineheight="['15','15','14','13']"
+                        data-fontweight="['400','400','400','600']"
+                        data-color="['rgb(0, 214, 163)','rgb(0, 214, 163)','rgb(0, 214, 163)','rgb(0, 214, 163)']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-visibility="['on','on','on','off']"
+                        data-type="text"
+                        data-actions=''
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":1570,"speed":300,"frame":"0","from":"x:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]"><u>Learn More</u></a>
+
+                        <a class="tp-caption white-flatborder-button tp-resizeme" href="#" target="_self" id="slide-6-layer-6" data-x="['left','left','left','center']" data-hoffset="['50','40','30','0']" data-y="['top','top','top','middle']" data-voffset="['467','482','319','89']"
+                        data-fontsize="['13','13','13','12']"
+                        data-lineheight="['16','16','16','12']"
+                        data-fontweight="['600','600','600','600']"
+                        data-letterspacing="['1','1','0','0']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+                        data-type="text"
+                        data-actions=''
+                        data-responsive_offset="on"
+                        data-frames='[{"delay":1480,"speed":500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power0.easeIn"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                        data-textAlign="['inherit','inherit','inherit','inherit']"
+                        data-paddingtop="[17,17,17,15]"
+                        data-paddingright="[30,30,23,20]"
+                        data-paddingbottom="[17,17,17,15]"
+                        data-paddingleft="[30,30,23,20]">MAKE AN APPOINTMENT! </a>
+
+                    </li>
+
+
+
+                    <!-- <li data-index="rs-8" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 
                         <img src="/img/slide1.png" alt="" title="mainslider-bg004" width="1918" height="710" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 
@@ -126,9 +316,9 @@
                             data-paddingleft="[30,30,23,25]">SUBSCRIBE NOW
                         </a>
 
-                    </li>
+                    </li> -->
 
-                    <li data-index="rs-17" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                    <!-- <li data-index="rs-17" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 
                         <img src="/img/slide2.png" alt="" title="mainslider-bg003" width="1918" height="710" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 
@@ -264,7 +454,7 @@
                             data-paddingbottom="[0,0,0,0]"
                             data-paddingleft="[0,0,0,0]">HOW WE WORK 
                         </div>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </div>
